68a96f2e2e7a1c63dc70d49a3c141eb343c7d01c
MelieCas <vcasares5@gmail.com>
2024-06-28 12:29:41 +0200

    Visual Studio

--- TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e8f3b2ff90dbfb41a1627e7dd18ac26b7e3c33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d903d52936a5ddf50d8d614f167b9e7d2a6925")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
